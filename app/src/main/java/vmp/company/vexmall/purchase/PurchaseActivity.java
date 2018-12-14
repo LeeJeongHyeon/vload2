a1a6920f41ae8ec47341281669d868c321cacb92
EunChani <kimbh0905@gmail.com>
2018-12-14 17:29:22 +0900

    Retrofit & Vendor height fix

---
+++ app/src/main/java/vmp/company/vexmall/purchase/PurchaseActivity.java
@@ -0,0 +1,15 @@
+package vmp.company.vexmall.purchase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vmp.company.vexmall.R;
+
+public class PurchaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase);
+    }
+}
--- app/src/main/java/vmp/company/vexmall/vexmall_application.java
+++ app/src/main/java/vmp/company/vexmall/vexmall_application.java
@@ -4,7 +4,27 @@ import android.app.Application;
 
 import com.igaworks.IgawCommon;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class vexmall_application extends Application {
+    private static Retrofit retrofit = null;
+
+    public Retrofit getRetrofit() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://vmp.company/vexMall/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
     @Override
     public void onCreate(){
         super.onCreate();
